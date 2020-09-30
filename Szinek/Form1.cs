1de7845d5764f901051c1d73beb62c0384d9ff76
Barbara Szőlősi <szolosi.barbara@dszcberegszaszi.hu>
2020-09-30 12:03:10 +0200

    1.0

---
+++ Szinek/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szinek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.BackColor = Color.FromName(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.ForeColor = Color.FromName(textBox2.Text);
+        }
+    }
+}
